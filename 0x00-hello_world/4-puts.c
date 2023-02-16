8774a0f7d88080b905fd56082e6143b48ce02faa
elormcent <innocent.elorm.fiadu@gmail.com>
2023-02-16 08:56:05 -0800

    print_using_puts_funtion

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * int  main() is the  entry point
+ * return is  Always 0 due to intructions and the void returns
+ * program is to -> print Programming is like building a multilingual puzzle
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
+}
