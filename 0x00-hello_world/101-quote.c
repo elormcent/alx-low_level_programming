8e68a0dc14a1a45f7967b1c5874e11a61f2877a2
elormcent <innocent.elorm.fiadu@gmail.com>
2023-02-16 09:50:46 -0800

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<unistd.h>
+/**
+ * main - point of entry
+ * Return: 1 is returned as a standard error.
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
