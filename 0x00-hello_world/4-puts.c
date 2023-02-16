4324279475ec23cbb96d38abe7804bfaa0096931
elormcent <innocent.elorm.fiadu@gmail.com>
2023-02-16 09:01:15 -0800

    print_using_puts_funtion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * int  main() is the  entry point
- * return is  Always 0 due to intructions and the void returns
- * program is to -> print Programming is like building a multilingual puzzle
+ * main -  is the  entry point
+ * return: is  Always 0 for success
  */
 
 int main(void)
