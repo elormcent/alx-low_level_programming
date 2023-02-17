2ca0f5d65a323d1ae8d2917928869d427e394876
elormcent <innocent.elorm.fiadu@gmail.com>
2023-02-17 14:28:47 -0800

    numberz

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point of the application
+ * Return: returns 0 for success
+ */
+int main(void)
+{
+char base_16;
+
+for (base_16 = 0; base_16 <= 10; base_16++)
+{
+	putchar(base_16 % 10 + '0');
+}
+
+for (base_16 = 'a' ; base_16 <= 'f'; base_16++)
+{
+	putchar(base_16);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
