ecbd21a5628ddf4cde2573554cee9c494b10b917
elormcent <innocent.elorm.fiadu@gmail.com>
2023-02-17 14:20:41 -0800

    Z-A

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point of the application
+ * Return: returns 0 for success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
