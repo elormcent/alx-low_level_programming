89e99762267ead1af59120291a27376e2bc8ce02
elormcent <innocent.elorm.fiadu@gmail.com>
2023-02-21 09:49:33 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 0 for success
+ */
+
+int main(void)
+{
+	char a_word[] = "_putchar";
+	int l;
+
+	for (l = 1; l <= 8; l++)
+		_putchar(a_word[ch]);
+		_putchar('\n');
+
+	return (0);
+}
