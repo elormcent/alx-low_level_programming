fd0871f0f9ed905d109862879883ac482864ca9c
elormcent <innocent.elorm.fiadu@gmail.com>
2023-02-21 10:01:12 -0800

    putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,27 @@
+# 1 "0-putchar.c"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 31 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 32 "<command-line>" 2
+# 1 "0-putchar.c"
+# 1 "main.h" 1
+# 2 "0-putchar.c" 2
+
+
+
+
+
+
+
+int main(void)
+{
+ char a_word[] = "_putchar";
+ int l;
+
+ for (l = 1; l <= 8; l++)
+  _putchar(a_word[ch]);
+  _putchar('\n');
+
+ return (0);
+}
