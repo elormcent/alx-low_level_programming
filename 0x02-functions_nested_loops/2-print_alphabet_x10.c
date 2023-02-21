253bca69a4a8ddd9c8cc132e90dcd961149941b1
elormcent <innocent.elorm.fiadu@gmail.com>
2023-02-21 11:03:35 -0800

    print_a_to_z_10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,20 @@
 #include"main.h"
 
 /**
- * print_alphabet_x10 - function that prints alphabet a to z 10 times on a new line each
+ * print_alphabet_x10 -  prints alphabet a to z 10 times on a new line each
  */
 
 void print_alphabet_x10(void)
 {
 	int line, ch;
+
 	line = 0;
 
 	while (line < 10)
 	{
 		line++;
 		for (ch = 'a'; ch <= 'z'; ++ch)
-		_putchar(ch);
+			_putchar(ch);
 		_putchar('\n');
 	}
 }
