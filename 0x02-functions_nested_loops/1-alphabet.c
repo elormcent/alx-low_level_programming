1b54fdecd09807fb7592b9132a7fcb2c939bbc55
elormcent <innocent.elorm.fiadu@gmail.com>
2023-02-21 10:41:54 -0800

    print_a_to_z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * a_z - a function that prints lowercas alphabet from a to z
+ */
+
+void print_alphabet(void)
+{
+	int l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	_putchar(l);
+	_putchar('\n');
+}
