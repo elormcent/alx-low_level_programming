1d474199eb2bd89129f8c59794bced4a87d905ed
elormcent <innocent.elorm.fiadu@gmail.com>
2023-02-23 13:20:49 -0800

    auto_push

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,14 +2,14 @@
 
 /**
  * _isdigit - Check if a character is a digit
- * @x: The number to be checked
+ * @c: The number to be checked
  * Return: 1 digit or 0 for any
  */
 
 int _isdigit(int c)
 {
 
-	if (c > -1 && c < 10)
+	if (c >= 48 && c <= 57)
 	{
 	return (1);
 	}
